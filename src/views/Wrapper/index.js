35c68e12d745fa17ef4d0295a89aa4d639a92323
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-09-16 20:42:44 +0900

    removed unnecessary social links

--- src/views/Wrapper/index.js
+++ src/views/Wrapper/index.js
@@ -215,7 +215,7 @@ class Wrapper extends React.PureComponent {
             <div className="md:flex md:items-center md:justify-between py-1 border-t border-gray-200">
               {/* Social links */}
               <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0 justify-center">
-                <li>
+                {/* <li>
                   <Link
                     to="#"
                     className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
@@ -259,10 +259,10 @@ class Wrapper extends React.PureComponent {
                       <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
                     </svg>
                   </Link>
-                </li>
+                </li> */}
               </ul>
               {/* Social links */}
-              <div className="flex mb-4 md:order-2 md:ml-4 md:mb-0 justify-end items-center">
+              <div className="flex mb-4 md:order-2 md:ml-4 md:mb-0 justify-center items-center">
                 <div className="font-poppins mr-4 text-md">
                   <div
                     className="px-4 cursor-pointer"
