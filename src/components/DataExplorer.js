68b00365c3a730c3f2243c9b2443dd9caf5867f7
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-10-12 14:08:44 +0900

    fixed fantom scan link issue

--- src/components/DataExplorer.js
+++ src/components/DataExplorer.js
@@ -1,81 +1,81 @@
-import React from 'react'
-import services from 'services'
+import React from "react";
+import services from "services";
 
 class DataExplorer extends React.PureComponent {
   constructor(props) {
-    super(props)
-    this.getFNS()
+    super(props);
+    this.getFNS();
 
     this.vendors = {
       // block explorers
       ftmscan: {
-        name: 'Fantom Scan',
-        logo: services.linking.static('images/vendor/ftmscan.png'),
-        class: 'h-12 w-12',
+        name: "Fantom Scan",
+        logo: services.linking.static("images/vendor/ftmscan.png"),
+        class: "h-12 w-12"
       },
       vscout: {
-        name: 'VScout',
-        logo: services.linking.static('images/vendor/vscout.svg'),
-        class: 'h-12 w-12',
+        name: "VScout",
+        logo: services.linking.static("images/vendor/vscout.svg"),
+        class: "h-12 w-12"
       },
 
       // dns & ip
       dnslookup: {
-        name: 'DNS-Lookup',
-        logo: services.linking.static('images/vendor/dns-lookup.png'),
-        class: 'h-12 w-12',
+        name: "DNS-Lookup",
+        logo: services.linking.static("images/vendor/dns-lookup.png"),
+        class: "h-12 w-12"
       },
 
       ipinfo: {
-        name: 'ipinfo.io',
-        logo: services.linking.static('images/vendor/ipinfo-io.png'),
-        class: 'h-12 w-12',
+        name: "ipinfo.io",
+        logo: services.linking.static("images/vendor/ipinfo-io.png"),
+        class: "h-12 w-12"
       },
 
       // ipfs
       ipfs: {
-        name: 'ipfs.io',
-        logo: services.linking.static('images/vendor/ipfs.png'),
-        class: 'h-12 w-12',
+        name: "ipfs.io",
+        logo: services.linking.static("images/vendor/ipfs.png"),
+        class: "h-12 w-12"
       },
       cloudflare: {
-        name: 'Cloudflare',
-        logo: services.linking.static('images/vendor/cloudflare.svg'),
-        class: 'h-12 w-12',
+        name: "Cloudflare",
+        logo: services.linking.static("images/vendor/cloudflare.svg"),
+        class: "h-12 w-12"
       },
 
       // social
       telegram: {
-        name: 'telegram',
-        logo: services.linking.static('images/vendor/telegram.png'),
-        class: 'h-12 w-12',
+        name: "telegram",
+        logo: services.linking.static("images/vendor/telegram.png"),
+        class: "h-12 w-12"
       },
       twitter: {
-        name: 'twitter',
-        logo: services.linking.static('images/vendor/twitter.png'),
-        class: 'h-12 w-12',
+        name: "twitter",
+        logo: services.linking.static("images/vendor/twitter.png"),
+        class: "h-12 w-12"
       },
       facebook: {
-        name: 'facebook',
-        logo: services.linking.static('images/vendor/facebook.png'),
-        class: 'h-12 w-12',
+        name: "facebook",
+        logo: services.linking.static("images/vendor/facebook.png"),
+        class: "h-12 w-12"
       },
       email: {
-        name: 'email',
-        logo: services.linking.static('images/vendor/gmail.png'),
-        class: 'h-12 w-12',
-      },
-    }
+        name: "email",
+        logo: services.linking.static("images/vendor/gmail.png"),
+        class: "h-12 w-12"
+      }
+    };
   }
 
   async getFNS() {
-    const api = await services.provider.buildAPI()
-    this.fns = api.fns
+    const api = await services.provider.buildAPI();
+    this.fns = api.fns;
   }
 
   renderVendor(key, getLink) {
-    const vendor = this.vendors[key]
-    const link = getLink(this.props.data.data)
+    const vendor = this.vendors[key];
+    const link = getLink(this.props.data.data);
     return (
       <a
         target="_blank"
@@ -85,76 +85,78 @@ class DataExplorer extends React.PureComponent {
         <div className={`${vendor.class} flex items-center justify-center`}>
           <img src={vendor.logo} alt={vendor.name} className="w-full" />
         </div>
-        <div className="mt-2">{vendor.name}</div>
+        <div className="mt-2">
+          {vendor.name}
+        </div>
       </a>
-    )
+    );
   }
 
   renderEVM() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
         {this.renderVendor(
-          'ftmscan',
-          (d) => `https://testnet.ftmscan.com/address/${d}`,
+          "ftmscan",
+          d => `https://ftmblocks.com/address/${d}`
         )}
       </div>
-    )
+    );
   }
 
   renderValidator() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
         {this.renderVendor(
-          'ftmscan',
-          (d) => `https://fantom.foundation/staking/validator/${d}`,
+          "ftmscan",
+          d => `https://fantom.foundation/staking/validator/${d}`
         )}
-        {this.renderVendor('vscout', (d) => `https://vscout.io/validator/${d}`)}
+        {this.renderVendor("vscout", d => `https://vscout.io/validator/${d}`)}
       </div>
-    )
+    );
   }
 
   renderDNS_A() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
-        {this.renderVendor('ipinfo', (d) => `https://ipinfo.io/${d}`)}
+        {this.renderVendor("ipinfo", d => `https://ipinfo.io/${d}`)}
       </div>
-    )
+    );
   }
 
   renderDNS_CNAME() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
-        {this.renderVendor('dnslookup', (d) => `https://dns-lookup.com/${d}`)}
+        {this.renderVendor("dnslookup", d => `https://dns-lookup.com/${d}`)}
       </div>
-    )
+    );
   }
   renderTelegram() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
-        {this.renderVendor('telegram', (d) => `https://t.me/${d}`)}
+        {this.renderVendor("telegram", d => `https://t.me/${d}`)}
       </div>
-    )
+    );
   }
   renderTwitter() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
-        {this.renderVendor('twitter', (d) => `http://twitter.com/${d}`)}
+        {this.renderVendor("twitter", d => `http://twitter.com/${d}`)}
       </div>
-    )
+    );
   }
   renderFaceBook() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
-        {this.renderVendor('facebook', (d) => `http://facebook.com//${d}`)}
+        {this.renderVendor("facebook", d => `http://facebook.com//${d}`)}
       </div>
-    )
+    );
   }
   renderEmail() {
     return (
       <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
-        {this.renderVendor('email', (d) => `mailTo:${d}`)}
+        {this.renderVendor("email", d => `mailTo:${d}`)}
       </div>
-    )
+    );
   }
   renderAvatar() {
     return (
@@ -168,91 +170,91 @@ class DataExplorer extends React.PureComponent {
           <div className="mt-4">View image</div>
         </a>
       </div>
-    )
+    );
   }
 
   renderContent() {
-    const data = this.props.data.data
-    const proto = data.split('://')[0]
-    const getIPFSHash = (d) => d.split('://')[1]
+    const data = this.props.data.data;
+    const proto = data.split("://")[0];
+    const getIPFSHash = d => d.split("://")[1];
 
-    if (proto === 'ipfs')
+    if (proto === "ipfs")
       return (
         <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-4">
           {this.renderVendor(
-            'ipfs',
-            (d) => `https://ipfs.io/ipfs/${getIPFSHash(d)}`,
+            "ipfs",
+            d => `https://ipfs.io/ipfs/${getIPFSHash(d)}`
           )}
           {this.renderVendor(
-            'cloudflare',
-            (d) => `https://cloudflare-ipfs.com/ipfs/${getIPFSHash(d)}`,
+            "cloudflare",
+            d => `https://cloudflare-ipfs.com/ipfs/${getIPFSHash(d)}`
           )}
         </div>
-      )
+      );
 
-    return <div className="text-center py-4">Unknown content protocol</div>
+    return <div className="text-center py-4">Unknown content protocol</div>;
   }
 
   renderLinks() {
-    const records = this.fns.RECORDS
+    const records = this.fns.RECORDS;
     switch (this.props.data.dataType) {
       case records.EVM:
-        return this.renderEVM()
+        return this.renderEVM();
 
       case records.VALIDATOR:
-        return this.renderValidator()
+        return this.renderValidator();
 
       case records.DNS_CNAME:
-        return this.renderDNS_CNAME()
+        return this.renderDNS_CNAME();
 
       case records.DNS_A:
-        return this.renderDNS_A()
+        return this.renderDNS_A();
 
       case records.AVATAR:
-        return this.renderAvatar()
+        return this.renderAvatar();
 
       case records.DISCRIPTION:
-        return this.renderContent()
+        return this.renderContent();
 
       case records.TELEGRAM:
-        return this.renderTelegram()
+        return this.renderTelegram();
 
       case records.FACEBOOK:
-        return this.renderFaceBook()
+        return this.renderFaceBook();
 
       case records.TWITTER:
-        return this.renderTwitter()
+        return this.renderTwitter();
 
       case records.EMAIL:
-        return this.renderEmail()
+        return this.renderEmail();
     }
-    return null
+    return null;
   }
 
   getTitle() {
-    const records = this.fns.RECORDS
+    const records = this.fns.RECORDS;
     let title = {
-      [records.X_CHAIN]: 'View on Block Explorer',
-      [records.P_CHAIN]: 'View on Block Explorer',
-      [records.EVM]: 'View on Block Explorer',
-      [records.VALIDATOR]: 'View on Node Explorer',
-      [records.DNS_CNAME]: 'DNS Information',
-      [records.DNS_A]: 'IP Address Information',
-      [records.AVATAR]: 'Preview Avatar',
-      [records.DESCRIPTION]: 'Open on IPFS Gateway',
-      [records.TELEGRAM]: 'Open on Telegram',
-      [records.TWITTER]: 'Open on Twitter',
-      [records.FACEBOOK]: 'Open on Facebook',
-      [records.EMAIL]: 'Open Email',
-    }[this.props.data.dataType]
-    if (!title) title = 'External Link'
-    return title
+      [records.X_CHAIN]: "View on Block Explorer",
+      [records.P_CHAIN]: "View on Block Explorer",
+      [records.EVM]: "View on Block Explorer",
+      [records.VALIDATOR]: "View on Node Explorer",
+      [records.DNS_CNAME]: "DNS Information",
+      [records.DNS_A]: "IP Address Information",
+      [records.AVATAR]: "Preview Avatar",
+      [records.DESCRIPTION]: "Open on IPFS Gateway",
+      [records.TELEGRAM]: "Open on Telegram",
+      [records.TWITTER]: "Open on Twitter",
+      [records.FACEBOOK]: "Open on Facebook",
+      [records.EMAIL]: "Open Email"
+    }[this.props.data.dataType];
+    if (!title) title = "External Link";
+    return title;
   }
 
   render() {
-    if (!this.props.data) return null
-    if (!this.fns) return null
-    const title = this.getTitle()
+    if (!this.props.data) return null;
+    if (!this.fns) return null;
+    const title = this.getTitle();
 
     return (
       <div>
@@ -261,8 +263,8 @@ class DataExplorer extends React.PureComponent {
         </div>
         {this.renderLinks()}
       </div>
-    )
+    );
   }
 }
 
-export default DataExplorer
+export default DataExplorer;
